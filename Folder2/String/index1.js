d35171485297f90cc2a36fb53ee1842a6847bec4
reetuTheCoder <codewithreetu@gmail.com>
2025-10-23 17:24:33 +0530

    str first qst solve

---
+++ Folder2/String/index1.js
@@ -0,0 +1,11 @@
+let str = "hello";
+console.log(str.split("").reverse().join(""));
+
+function reversedStr(str) {
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
+console.log(reversedStr(str));
