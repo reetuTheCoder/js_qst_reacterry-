2ca88e3663527d6e886b08a3821ef8777c84b579
reetuTheCoder <codewithreetu@gmail.com>
2025-10-23 18:10:27 +0530

    second and third

--- Folder2/INTERMEDIATE/index1.js
+++ Folder2/INTERMEDIATE/index1.js
@@ -1,4 +1,44 @@
-let str = "rreetu"
+let str = "rreetu";
 
-console.log(str.split("").find((ele) => str.indexOf(ele) === str.lastIndexOf(ele)));
+console.log(
+  str.split("").find((ele) => str.indexOf(ele) === str.lastIndexOf(ele))
+);
 
+function frequencyCount(str) {
+  let feq = {};
+
+  for (const char of str) {
+    feq[char] = (feq[char] || 0) + 1;
+  }
+
+  return feq;
+}
+
+console.log(frequencyCount("banana"));
+
+let arr1 = [1, 2, 3];
+let arr2 = [2, 3, 4];
+
+console.log([...new Set([...arr1, ...arr2])]);
+
+// function mergeArryWithoutDuplicates(arr1, arr2) {
+//     let combined = [...arr1, ...arr2];
+//   return combined.filter((num, index) => combined.indexOf(num) === index);
+// }
+
+// console.log(mergeArryWithoutDuplicates(arr1, arr2));
+
+function mergeArryWithoutDuplicates(arr1, arr2) {
+  let arr = [...arr1];
+//   console.log(arr);
+
+  arr2.forEach((ele)=> {
+    if (!arr.includes(ele)) {
+        arr.push(ele)
+    }
+  })
+
+  return arr
+}
+
+console.log(mergeArryWithoutDuplicates(arr1, arr2));
