23e942fb576bb2b0d9a3987768f24d3a79c22849
reetuTheCoder <codewithreetu@gmail.com>
2025-10-25 14:39:28 +0530

    revese str

---
+++ fileBaseQst/stringReverse.js
@@ -0,0 +1,25 @@
+let str = "hello";
+
+console.log(str.split("").reverse().join(""));
+
+function strReve(str) {
+  let output = "";
+
+  for (let i = 0; i < str.length - 1; i--) {
+    output += str[i];
+  }
+
+  return output;
+}
+console.log(strReve(str));
+
+function strReve(str) {
+  let output = "";
+
+  for (const char of str) {
+    output = char + output;
+  }
+
+  return output;
+}
+console.log(strReve(str));
