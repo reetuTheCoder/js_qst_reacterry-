0283a4d36ee482db6d34cdd91638459bde638a7c
reetuTheCoder <codewithreetu@gmail.com>
2025-10-25 12:22:53 +0530

    solve 3 4 5 qst

--- Folder2/INTERMEDIATE/index1.js
+++ Folder2/INTERMEDIATE/index1.js
@@ -1,8 +1,8 @@
 let str = "rreetu";
 
-console.log(
-  str.split("").find((ele) => str.indexOf(ele) === str.lastIndexOf(ele))
-);
+// console.log(
+//   str.split("").find((ele) => str.indexOf(ele) === str.lastIndexOf(ele))
+// );
 
 function frequencyCount(str) {
   let feq = {};
@@ -19,7 +19,7 @@ console.log(frequencyCount("banana"));
 let arr1 = [1, 2, 3];
 let arr2 = [2, 3, 4];
 
-console.log([...new Set([...arr1, ...arr2])]);
+// console.log([...new Set([...arr1, ...arr2])]);
 
 // function mergeArryWithoutDuplicates(arr1, arr2) {
 //     let combined = [...arr1, ...arr2];
@@ -30,15 +30,87 @@ console.log([...new Set([...arr1, ...arr2])]);
 
 function mergeArryWithoutDuplicates(arr1, arr2) {
   let arr = [...arr1];
-//   console.log(arr);
+  //   console.log(arr);
 
-  arr2.forEach((ele)=> {
+  arr2.forEach((ele) => {
     if (!arr.includes(ele)) {
-        arr.push(ele)
+      arr.push(ele);
     }
-  })
+  });
 
-  return arr
+  return arr;
 }
 
-console.log(mergeArryWithoutDuplicates(arr1, arr2));
+// console.log(mergeArryWithoutDuplicates(arr1, arr2));
+
+let nestedArr = [1, [2, [3, 4]], 5];
+
+// console.log(nestedArr);
+// console.log(nestedArr.flat(Infinity));
+
+function flatten(arr) {
+  const stack = [...arr];
+  const result = [];
+  console.log("stackstack", stack);
+
+  while (stack.length) {
+    // console.log("stack.length", stack.length);
+    const storeCutElm = stack.pop();
+    if (Array.isArray(storeCutElm)) {
+      stack.push(...storeCutElm);
+      // console.log("fijfj", stack);
+      // console.log("storeCutElm", storeCutElm);
+    } else {
+      result.push(storeCutElm);
+    }
+  }
+
+  console.log("result ::", result);
+
+  return result.reverse();
+}
+console.log(flatten([1, [2, [3, [4, [5, [6, [7]]]]]]]));
+
+// 5. Find Common Elements in Two Arrays
+
+let arr3 = [1, 2, 3];
+let arr4 = [2, 3, 4, 1];
+
+let commonArr = [arr3, arr4];
+// console.log(commonArr);
+
+// function commonEle(arr) {
+//   return arr.reduce((acc, cVal) =>
+//     acc.filter((ele) => cVal.includes(ele), [0])
+//   );
+// }
+
+// console.log("fdgdg", commonEle(commonArr));
+
+function commonEle(arr) {
+  console.log("arr val", arr);
+  
+  let feq = {};
+  let output = [];
+  let flatArr = arr.flat();
+  console.log("flatArr", flatArr.length);
+
+  for (const num of flatArr) {
+    feq[num] = (feq[num] || 0) + 1;
+
+    console.log("feq[num] ", feq[num]);
+  }
+
+  for (const key in feq) {
+    console.log("key", key);
+    if (feq[key] === commonArr.length) {
+      output.push(Number(key));
+    }
+  }
+
+  return output;
+}
+
+console.log("fdgdg", commonEle(commonArr));
+
+
