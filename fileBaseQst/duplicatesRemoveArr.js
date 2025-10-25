8957268a1bc70ea8e56cac5c1a14de5443450b4f
reetuTheCoder <codewithreetu@gmail.com>
2025-10-25 17:30:36 +0530

    remove duplicates

--- fileBaseQst/duplicatesRemoveArr.js
+++ fileBaseQst/duplicatesRemoveArr.js
@@ -1,13 +1,29 @@
 // 3. Remove duplicates
 
-let arr = [1, 2, 2, 3, 4, 4]
+let arr = [1, 2, 2, 3, 4, 4, 5];
 
 function removeDuplicat(arr) {
-   return arr.filter((ele, index)=> arr.indexOf(ele) === index)
+  return arr.filter((ele, index) => arr.indexOf(ele) === index);
 }
+// console.log(removeDuplicat(arr));
+
+function removeDuplicat(arr) {
+ return [...new Set(arr)]
+}
+
 // console.log(removeDuplicat(arr));
 
 
 function removeDuplicat(arr) {
-   return arr.filter((ele, index)=> arr.indexOf(ele) === index)
+ let duplicates = []
+
+ for (const num of arr) {
+    if (!duplicates.includes(num)) {
+        duplicates.push(num)
+    }
+ }
+
+ return duplicates
 }
+
+console.log(removeDuplicat(arr));
